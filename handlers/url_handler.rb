94ea02e2fe14181de00353dc46581412e3fb8cf0
epitron <chris@ill-logic.com>
2009-10-19 01:37:38 -0400

    curl mode!

--- handlers/url_handler.rb
+++ handlers/url_handler.rb
@@ -2,8 +2,9 @@
 # go along - e.g. dump events etc.
 # options = {:ident=>"i=epi", :host=>"unaffiliated/epitron", :nick=>"Epilogue", :message=>"asdf", :target=>"#pookie-testing"}
 
-require 'net/http'
-require 'uri'
+#require 'net/http'
+#require 'uri'
+require 'curb'
 require 'cgi'
 
 class UrlHandler < Marvin::CommandHandler
@@ -23,6 +24,32 @@ class UrlHandler < Marvin::CommandHandler
       '#8221' => '"',
       '#8212' => '--',
       '#39' => '\'',
+  }
+  
+  HTTP_STATUS_CODES = {
+    000 => "Incomplete/Undefined error",
+    201 => "Created",
+    202 => "Accepted",
+    203 => "Partial Information",
+    204 => "Page does not contain any information",
+    204 => "No response",
+    206 => "Only partial content delivered",
+    300 => "Page redirected",
+    301 => "Permanent URL relocation",
+    302 => "Temporary URL relocation",
+    303 => "Temporary relocation method and URL",
+    304 => "Document not modified",
+    400 => "Bad request (syntax)",
+    401 => "Unauthorized access (requires authentication)",
+    402 => "Access forbidden (payment required)",
+    403 => "Forbidden",
+    404 => "URL not found",
+    405 => "Method not Allowed (Most likely the result of a corrupt CGI script)",
+    408 => "Request time-out",
+    500 => "Internet server error",
+    501 => "Functionality not implemented",
+    502 => "Bad gateway",
+    503 => "Service unavailable",
   }
 
   ### Handle All Lines of Chat ############################
@@ -51,7 +78,89 @@ class UrlHandler < Marvin::CommandHandler
 
   ### Private methods... ###############################
   
-  def get_title_for_url(uri_str, depth=10)
+  def commatize(thing)
+    thing.to_i.to_s.gsub(/(\d)(?=\d{3}+(?:\.|$))(\d{3}\..*)?/,'\1,\2')
+  end
+  
+  def get_title_for_url(url, depth=10, max_bytes=30000)
+    
+    easy = Curl::Easy.new(url) do |c|
+      # Gotta put yourself out there...
+      c.headers["User-Agent"] = "Curl/Ruby"
+
+      c.verbose = true
+     
+      c.follow_location = true
+      c.max_redirects = depth
+
+      c.enable_cookies = true
+      c.cookiefile = "/tmp/curb.cookies"
+    end
+    
+    logger.debug "[get_title_for_url] HEAD #{url}"
+    
+    begin
+      easy.http_head
+    rescue Exception => e
+      return "[Link Info] Error: #{e}"
+    end
+    
+    code = easy.response_code
+    unless code == 200 or code == 403
+      return "[Link Info] Error: #{code} #{HTTP_STATUS_CODES[code]}"
+    end
+    
+    ### HTML page
+    if easy.content_type =~ /^text\//
+
+      data = ""
+      easy.on_body{|chunk| data << chunk; data.size < max_bytes ? chunk.size : 0 }
+      begin
+        logger.debug "[get_title_for_url] GET #{url}"
+        easy.url = easy.last_effective_url
+        easy.perform
+      rescue Exception => e
+        logger.debug "RESCUED #{e.inspect}"
+      end
+      #data = easy.body_str
+      return get_title_from_html(data)
+    
+    ### Binary file
+    else
+      # content doesn't have title, just display info.
+      size = easy.downloaded_content_length #request_size
+      return "[Link Info] type: #{easy.content_type}#{size ? ", size: #{commatize(size)} bytes" : ""}"
+    end
+    
+  end
+  
+  def get_title_from_html(pagedata)
+    return unless TITLE_RE.match(pagedata)
+    title = $1.strip.gsub(/\s*\n+\s*/, " ")
+    title = unescape_title title
+    title = title[0..255] if title.length > 255
+    "[Link Info] title: #{title}"
+  end
+
+  def unescape_title(htmldata)
+    # first pass -- let CGI try to attack it...
+    htmldata = CGI::unescapeHTML htmldata
+    
+    # second pass -- destroy the remaining bits...
+    htmldata.gsub(/(&(.+?);)/) {
+        symbol = $2
+        
+        # remove the 0-paddng from unicode integers
+        if symbol =~ /#(.+)/
+            symbol = "##{$1.to_i.to_s}"
+        end
+        
+        # output the symbol's irc-translated character, or a * if it's unknown
+        UNESCAPE_TABLE[symbol] || '*'
+    }
+  end
+
+  def get_title_for_url2(uri_str, depth=10)
     # This god-awful mess is what the ruby http library has reduced me to.
     # Python's HTTP lib is so much nicer. :~(
     
@@ -131,34 +240,6 @@ class UrlHandler < Marvin::CommandHandler
   end
 
 
-  def unescape_title(htmldata)
-    # first pass -- let CGI try to attack it...
-    htmldata = CGI::unescapeHTML htmldata
-    
-    # second pass -- destroy the remaining bits...
-    htmldata.gsub(/(&(.+?);)/) {
-        symbol = $2
-        
-        # remove the 0-paddng from unicode integers
-        if symbol =~ /#(.+)/
-            symbol = "##{$1.to_i.to_s}"
-        end
-        
-        # output the symbol's irc-translated character, or a * if it's unknown
-        UNESCAPE_TABLE[symbol] || '*'
-    }
-  end
-
-
-  def get_title_from_html(pagedata)
-    return unless TITLE_RE.match(pagedata)
-    title = $1.strip.gsub(/\s*\n+\s*/, " ")
-    title = unescape_title title
-    title = title[0..255] if title.length > 255
-    "[Link Info] title: #{title}"
-  end
-
-  
   #exposes :what
   
   #def what(*args)
